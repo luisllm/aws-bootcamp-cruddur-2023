824ab0f9ebbc40a757730e2240c2976203b3e408
luisllm <luis.longo.m@gmail.com>
2023-03-26 21:12:03 +0000

    week5 changes

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,9 +1,10 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, handle, email, cognito_user_id)
 VALUES
-  ('Andrew Brown', 'andrewbrown' , 'andrewbrown@example.co' , 'MOCK'),
+  ('Luis Longo','llm', 'luis.longo.m@gmail.com', 'MOCK'),
   ('Andrew Bayko', 'bayko' , 'bayko@example.co' , 'MOCK'),
-  ('Luis Longo','llm', 'luis.longo.m@gmail.com', 'MOCK');
+  ('Londo Mollari', 'londo' , 'lmollari@centari.co' , 'MOCK');
+  
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
