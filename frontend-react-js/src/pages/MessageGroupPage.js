a9f43f42e1d95a665c19306ded003dda33fbf336
luisllm <luis.longo.m@gmail.com>
2023-04-04 10:44:02 +0000

    wip

--- frontend-react-js/src/pages/MessageGroupPage.js
+++ frontend-react-js/src/pages/MessageGroupPage.js
@@ -6,7 +6,7 @@ import DesktopNavigation  from '../components/DesktopNavigation';
 import MessageGroupFeed from '../components/MessageGroupFeed';
 import MessagesFeed from '../components/MessageFeed';
 import MessagesForm from '../components/MessageForm';
-import checkAuth from '../lib/CheckAuth';
+import {checkAuth, getAccessToken} from '../lib/CheckAuth';
 
 export default function MessageGroupPage() {
   const [messageGroups, setMessageGroups] = React.useState([]);
@@ -18,10 +18,13 @@ export default function MessageGroupPage() {
 
   const loadMessageGroupsData = async () => {
     try {
+      await getAccessToken()
+      const access_token = localStorage.getItem("access_token") 
+
       const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/message_groups`
       const res = await fetch(backend_url, {
         headers: {
-          Authorization: `Bearer ${localStorage.getItem("access_token")}`
+          'Authorization': `Bearer ${access_token}`,
         },
         method: "GET"
       });
@@ -38,10 +41,13 @@ export default function MessageGroupPage() {
 
   const loadMessageGroupData = async () => {
     try {
+      await getAccessToken()
+      const access_token = localStorage.getItem("access_token") 
+      
       const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/messages/${params.message_group_uuid}`
       const res = await fetch(backend_url, {
         headers: {
-          Authorization: `Bearer ${localStorage.getItem("access_token")}`
+          'Authorization': `Bearer ${access_token}`,
         },
         method: "GET"
       });
