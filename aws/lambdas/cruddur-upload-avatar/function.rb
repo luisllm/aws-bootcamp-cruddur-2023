1e7d8feb1d63e65a69330d396230d0b99dc7cc97
luisllm <luis.longo.m@gmail.com>
2023-04-18 17:35:33 +0000

    wip

--- aws/lambdas/cruddur-upload-avatar/function.rb
+++ aws/lambdas/cruddur-upload-avatar/function.rb
@@ -1,6 +1,16 @@
 require 'aws-sdk-s3'
 require 'json'
-require 'jwt'
+#require 'jwt'
+
+#s3 = Aws::S3::Resource.new
+#bucket_name = ENV["UPLOADS_BUCKET_NAME"]
+#object_key = "mock.jpg"
+#obj = s3.bucket(bucket_name).object(object_key)
+#url = obj.presigned_url(:put, expires_in: 3600)
+#puts url
+
+
+
 
 def handler(event:, context:)
   puts event
